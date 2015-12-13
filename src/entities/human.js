a48dfa9d896bd31794239d5c49b94b328bdd9923
Joseph Frangoudes <jfrangoudes@alertlogic.com>
2015-12-13 21:11:57 +0000

    Zombies and Humans bob up and down at different rates

--- src/entities/human.js
+++ src/entities/human.js
@@ -17,6 +17,30 @@ export default function(x, y, i, j) {
 
   human.isSafe = false;
 
+  human.animation = {
+    cycleFrames: 3,
+    counter: 0,
+    yPos: 0,
+
+    update: function() {
+      if(this.counter < this.cycleFrames) {
+        this.yPos -= 0.01;
+      } else {
+        this.yPos = 0;
+        this.counter = 0;
+      }
+
+      this.counter ++;
+    },
+
+    displaceSprite: function(x, y) { 
+      return {
+        y: y + this.yPos,
+        x: x
+      };
+    }
+  };
+
   human.item = randomProperty(items);
 
   return human;
