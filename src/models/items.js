48a28d3d56ffaa1e82b74ce01173a7cc3b082e28
danprince <danielprince@hotmail.co.uk>
2015-12-13 22:08:11 +0530

    add items and move behaviour to items file

--- src/models/items.js
+++ src/models/items.js
@@ -1,12 +1,55 @@
 import defineSprite from '../util/define-sprite';
 
 export default {
-  //Speeds up humans
-  Jet:     { itemSprite: defineSprite(0, 0), itemName: 'Jet' },
-
-  //slows down zombies
-  BrainFreeze: { itemSprite: defineSprite(0, 0), itemName: 'BrainFreeze' },
-
-  //no item
-  NoItem: { itemName: 'NoItem' }
+  creditCard: {
+    sprite: defineSprite(2, 1),
+    name: 'Credit Card'
+    apply: function(player, state) {
+      state.scoreMultipliers.push({ value: 2 });
+    }
+  },
+  redbull: {
+    sprite: defineSprite(2, 2),
+    name: 'Energy Drink'
+    apply: function(player, state) {
+      state.entities.forEach(entity => {
+        // speed all humans up
+      });
+    }
+  },
+  phone: {
+    sprite: defineSprite(2, 2),
+    name: 'Mobile Phone'
+    apply: function(player, state) {
+      player.score *= 10;
+    }
+  },
+  jackpot: {
+    sprite: defineSprite(2, 3),
+    name: 'Jackpot'
+    apply: function(player, state) {
+      state.entities.forEach(entity => {
+        // turn all entities into gentlemen
+      });
+    }
+  },
+  burger: {
+    sprite: defineSprite(2, 4),
+    name: 'Burger'
+    apply: function(player, state) {
+      state.entities.forEach(entity => {
+        // spawn slow & fat people
+      });
+    }
+  },
+  icecream: {
+    sprite: defineSprite(2, 5),
+    name: 'Brainfreeze'
+    apply: function(player, state) {
+      state.entities.forEach(entity => {
+        // slow all zombies
+      });
+    }
+  }
 }
+
