b03bb591b370332fa8295c90057f83a1eb994bb4
danprince <danielprince@hotmail.co.uk>
2015-12-15 03:30:15 +0530

    added more quotes and improved human points

--- src/models/quotes.js
+++ src/models/quotes.js
@@ -3,33 +3,41 @@ import Improvise from 'improvise';
 const grammar = Improvise.grammar({
   noun: ['bridge', 'zombie'],
   adjectives: ['wonderful', 'great', 'super'],
+  super: ['Super', 'Mega', 'Wonder', 'Cheese', 'Grand', 'GNU / '],
   difficulty: ['easy', 'tough', 'really difficult'],
+  mate: ['friend', 'mate', 'dude', 'guys', 'bro'],
+  L: ['Lonely', 'Lovely', 'Llama', 'Lite'],
+  insult: ['muppet', 'twonk', 'spanner', 'pancake', 'bitch'],
   snow: [
     'You know nothing, Jon {{noun}}',
-    'I am the watcher on the {{noun}}',
-    'The nights watch is {{adjective}}',
-    'It is {{difficulty}} being a bastard'
+    'I am the watcher on the {{noun}}'
   ],
   jules: [
-    'Does he look like a {{noun}} to you?',
+    'Does he look like a {{insult}} to you?',
     'This was divine intervention!',
-    'English, motherfucker. Do you speak it?',
-    'I\'ve had it with these {{nouns}} on this {{bridge]}!'
+    'English, motherf*cker. Do you speak it?',
+    'I\'ve had it with these {{noun}} on this {{noun}}!',
+    'That\'s Samuel {{L}} Jackson, to you!'
   ],
   jesus: [
     'My side hurts.',
     'Holy {{noun}}s!',
-    'Look busy bro!'
+    'Look busy {{mate}}!',
+    'I\'m literally {{super}} Jesus',
   ],
   gandalf: [
     'Fly you fools!',
     'A wizard is never late.',
-    'Fool of a {{noun}}!'
+    'Fool of a {{noun}}!',
+    'I\'m a tired developer',
+    'Peregrin Took, you {{insult}}!'
   ],
   jones: [
     'I hate snakes!',
+    'It belongs in a museum you {{insult}}!',
     'Why\'d it have to be {{noun}}s?',
-    '{{noun}}s belongs in a museum!'
+    '{{noun}}s belongs in a museum!',
+    'Thinking up quotes is hard'
   ]
 });
 
