d291e431094fac8f0e77d8d4dd3041155c082e09
danprince <danielprince@hotmail.co.uk>
2015-12-15 05:05:51 +0530

    refining quotes

--- src/models/quotes.js
+++ src/models/quotes.js
@@ -3,53 +3,56 @@ import Improvise from 'improvise';
 const grammar = Improvise.grammar({
   noun: ['bridge', 'zombie'],
   adjective: ['wonder', 'great', 'super', 'extra'],
-  very: ['very', 'quite', 'a bit'],
+  very: ['very', 'quite', 'slightly'],
+  bad: ['cheap', 'shit', 'naff', 'lame', 'crap', 'horrible'],
   super: ['super', 'mega', 'wonder', 'macho', 'grand', 'GNU / ', 'fucking', 'bloody'],
+  alcohol: ['WKD', 'Vodka', 'Jager', 'Absinthe'],
+  number: ['five', 'six', 'three', 'two'],
   difficulty: ['easy', 'tough', 'really difficult'],
   mate: ['friend', 'mate', 'dude', 'guys', 'bro'],
   L: ['Lonely', 'Larry', 'Llama', 'Lite', 'Little'],
   megainsult: '{{adjective}}{{insult}}',
   doubleinsult: '{{insult}}{{insult}}',
-  insult: ['{{megainsult}}', '{{doubleinsult}}', 'muppet', 'twonk', 'spanner',
-           'pancake', 'bitch', 'twat', 'dick', 'piglet', 'simple', 'idiot',
-           'boffin', 'sellout'],
+  insult: ['{{megainsult}}', 'muppet', 'knob', 'spanner', 'cretin',
+           'pancake', 'bitch', 'twat', 'dick', 'idiot'],
   snow: [
-    'You know nothing, Jon {{noun}}',
+    'You know nothing, Jon Snow',
     'I am the watcher on the {{noun}}'
   ],
   jules: [
     'Does he look like a {{insult}} to you?',
     'This was divine intervention!',
     'English, {{doubleinsult}}. Do you speak it?',
-    'I\'ve had it with these {{noun}} on this {{noun}}!',
-    'Samuel {{L}} Jackson',
-    'The L stands for {{L}}'
+    'I\'ve had it with these {{noun}}s on this {{noun}}!',
+    'Samuel \'{{L}}\' Jackson',
+    'The L stands for {{L}}',
+    'The path of the righteous man is beset on all sides'
   ],
   jesus: [
     'My side hurts.',
-    'The romans are {{megainsult}}s',
-    'Holy {{noun}}s!',
+    'The romans are {{insult}}s',
     'Look busy {{mate}}!',
-    'I do {{very}} {{difficulty}} miracles {{insult}}',
+    'I do {{difficulty}} miracles, {{insult}}',
     'I\'m literally {{super}} Jesus',
-    'God is {{adjective}}'
+    'I turn water into {{alcohol}}!',
+    'Judas is a {{insult}}',
+    'He\'s not the messiah!'
   ],
   gandalf: [
     'Fly you fools!',
     'Fly you {{insult}}s!',
     'A wizard is never late.',
     'Fool of a {{noun}}!',
-    'I\'m a tired developer',
     'Peregrin Took, you {{insult}}!',
     'Throw yourself in next time, you {{insult}}',
-    'I\'m not a conjurer of cheap tricks!'
+    'I\'m not a conjurer of {{bad}} tricks!'
   ],
   jones: [
     'I hate snakes!',
     'It belongs in a museum you {{insult}}!',
     'Why\'d it have to be {{noun}}s?',
     '{{noun}}s belongs in a museum!',
-    'Thinking up quotes is hard'
+    'All my quotes are {{bad}}'
   ]
 });
 
